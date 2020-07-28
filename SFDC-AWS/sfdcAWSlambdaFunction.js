d5441b9eb652dfbce0a0c2589d8f3c3ebf4954c2
ggit007 <57431105+ggit007@users.noreply.github.com>
2020-07-29 02:21:01 +0530

    Create sfdcAWSlambdaFunction.js

---
+++ SFDC-AWS/sfdcAWSlambdaFunction.js
@@ -0,0 +1,33 @@
+// to be written in AWS Lambda function
+
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+var tableName = 'sfdcdynamodbtable';
+ 
+exports.handler = (event, context, callback) => {
+    dynamodb.putItem({
+       // TableName: process.env.tableName,
+       TableName: tableName,
+        Item: {
+            "id": {
+                S: event.id
+            },
+            "Name": {
+                S: event.Name
+            }
+        }
+    }, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            callback(null, {
+                statusCode: '500',
+                body: err
+            });
+        } else {
+            callback(null, {
+                statusCode: '200',
+                body: 'Hello ' + event.id + event.Name + '!'
+            });
+        }
+    })
+};
